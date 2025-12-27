9e2fa8b134d30237a507e663615c3d493d91b8bc
Snowflake6413 <verycoolandsmart@protonmail.com>
2025-12-26 23:26:28 -0500

    add: contact page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let name = 'alexander';
+</script>
+
+<style>
+    .pfp-img {
+        border-radius: 50%;
+        border: 4px solid #8FA9A5;
+        width: 128px;
+        height: 128px;
+        object-fit: cover;
+    }
+</style>
+
+<div class="pfp stuff">
+    <img src="/slackpfp.jpg" alt="my profile pic on HC slack! :3" class="pfp-img">
+</div>
+
+
+
+<h1>{name}</h1>
+<p>my name is {name}</p>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Icon from "@iconify/svelte";
+    import '@fontsource/jetbrains-mono';
+</script>
+
+<style>
+    .maintext {
+        font-family: 'JetBrains Mono';
+    }
+
+    .NiceContainerforyouricons {
+        display: flex;
+        justify-content: center;
+        gap: 1rem ;
+    }
+</style>
+
+<h1 class="maintext">you can contact <s>stalk me</s> on these platforms</h1>
+
+<div class="NiceContainerforyouricons">
+<a href="https://hackclub.enterprise.slack.com/team/U09PHG7RLGG" target="_blank">
+    <Icon icon="mdi:slack" width="38" height="38"/>
+</a>
+<a href="https://hc-cdn.hel1.your-objectstorage.com/s/v3/6f8ad79a4625586f_static-qr-code-5c947467fc92a0e2f26bd654c8af44d9.svg" target="_blank">
+    <Icon icon="simple-icons:session" width="38" height="38"/>
+</a>
+</div>
